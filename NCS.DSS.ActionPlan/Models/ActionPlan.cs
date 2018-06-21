5837aa9cf09de27aca88fc2b14f4f9899905319a
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-21 15:20:14 +0100

    Add Example to API definition

--- NCS.DSS.ActionPlan/Models/ActionPlan.cs
+++ NCS.DSS.ActionPlan/Models/ActionPlan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using NCS.DSS.ActionPlan.Annotations;
 using NCS.DSS.ActionPlan.ReferenceData;
 
 namespace NCS.DSS.ActionPlan.Models
@@ -7,38 +8,47 @@ namespace NCS.DSS.ActionPlan.Models
     public class ActionPlan
     {
         [Display(Description = "Unique identifier of the action plan record.")]
+        [Example(Description = "b8592ff8-af97-49ad-9fb2-e5c3c717fd85")]
         public Guid ActionPlanId { get; set; }
 
         [Required]
         [Display(Description = "Unique identifier to the related interaction resource.")]
+        [Example(Description = "2730af9c-fc34-4c2b-a905-c4b584b0f379")]
         public Guid InteractionId { get; set; }
 
         [Required]
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time action plan was created.")]
+        [Example(Description = "2018-06-20T21:45:00")]
         public DateTime DateActionPlanCreated { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time the action plan was sent (or made available) to the customer.")]
+        [Example(Description = "2018-06-21T13:32:00")]
         public DateTime DateActionPlanSentToCustomer { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time the customer acknowledged receipt of the action plan.")]
+        [Example(Description = "2018-06-22T07:55:00")]
         public DateTime DateActionPlanAcknowledged { get; set; }
 
         [Required]
         [Display(Description = "Priority Customer reference data.")]
+        [Example(Description = "1")]
         public PriorityCustomer PriorityCustomer { get; set; }
 
         [StringLength(4000)]
         [Display(Description = "Summary of a customer current situation and how it affects their career.")]
+        [Example(Description = "this is some text")]
         public string CurrentSituation { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time of the last modification to the record.")]
+        [Example(Description = "2018-06-20T13:45:00")]
         public DateTime LastModifiedDate { get; set; }
 
         [Display(Description = "Identifier of the touchpoint who made the last change to the record")]
+        [Example(Description = "d1307d77-af23-4cb4-b600-a60e04f8c3df")]
         public Guid LastModifiedTouchpointId { get; set; }
     }
 }
