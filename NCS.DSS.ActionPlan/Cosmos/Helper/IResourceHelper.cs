70a3bd367e1007b6560ae65a5ac22145e7120097
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-13 10:46:55 +0100

    Add Cosmos to Action Plan

---
+++ NCS.DSS.ActionPlan/Cosmos/Helper/IResourceHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NCS.DSS.ActionPlan.Cosmos.Helper
+{
+    public interface IResourceHelper
+    {
+        bool DoesCustomerExist(Guid customerId);
+        bool DoesInteractionExist(Guid interactionId);
+    }
+}
---
+++ NCS.DSS.ActionPlan/Cosmos/Provider/IDocumentDBProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using NCS.DSS.ActionPlan.Models;
+
+namespace NCS.DSS.ActionPlan.Cosmos.Provider
+{
+    public interface IDocumentDBProvider
+    {
+        bool DoesCustomerResourceExist(Guid customerId);
+        bool DoesInteractionResourceExist(Guid interactionId);
+        Task<List<Models.ActionPlan>> GetActionPlansForCustomerAsync(Guid customerId);
+        Task<Models.ActionPlan> GetActionPlanForCustomerAsync(Guid customerId, Guid actionPlanId);
+        Task<ResourceResponse<Document>> CreateActionPlanAsync(Models.ActionPlan actionPlan);
+        Task<ResourceResponse<Document>> UpdateActionPlanAsync(Models.ActionPlan actionPlan);
+    }
+}
