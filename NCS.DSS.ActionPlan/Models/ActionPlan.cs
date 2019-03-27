dce813b7f3d541561ce1a4d83a1fdbc69bca4d3b
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-03-27 12:05:27 +0000

    remove html tags in description

--- NCS.DSS.ActionPlan/Models/ActionPlan.cs
+++ NCS.DSS.ActionPlan/Models/ActionPlan.cs
@@ -33,8 +33,8 @@ namespace NCS.DSS.ActionPlan.Models
 
         [Required]
         [DataType(DataType.DateTime)]
-        [Display(Description = "Date and time action plan was created. </br>" +
-                               "Validation Rules: </br>" +
+        [Display(Description = "Date and time action plan was created. " +
+                               "Validation Rules: " +
                                "DateActionPlanCreated >= Session.DateAndTimeOfSession")]
         [Example(Description = "2018-06-20T21:45:00")]
         public DateTime? DateActionPlanCreated { get; set; }
@@ -45,42 +45,42 @@ namespace NCS.DSS.ActionPlan.Models
         public bool? CustomerCharterShownToCustomer { get; set; }
 
         [DataType(DataType.DateTime)]
-        [Display(Description = "Date and time the customer was shown the customer charter. </br>" +
-                               "Validation Rules: </br>" +
+        [Display(Description = "Date and time the customer was shown the customer charter. " +
+                               "Validation Rules: " +
                                "DateAndTimeCharterShown >= DateActionPlanCreated")]
         [Example(Description = "2018-06-20T21:45:00")]
         public DateTime? DateAndTimeCharterShown { get; set; }
 
         [DataType(DataType.DateTime)]
-        [Display(Description = "Date and time the action plan was sent (or made available) to the customer. </br>" +
-                               "Validation Rules: </br>" +
+        [Display(Description = "Date and time the action plan was sent (or made available) to the customer. " +
+                               "Validation Rules: " +
                                "DateActionPlanSentToCustomer >= DateActionPlanCreated")]
         [Example(Description = "2018-06-21T13:32:00")]
         public DateTime? DateActionPlanSentToCustomer { get; set; }
 
-        [Display(Description = "Action Plan Delivery Method reference data. </br>" +
-                                "1 - Paper, </br>" +
-                                "2 - Email, </br>" +
-                                "3 - Digital, </br>" +
+        [Display(Description = "Action Plan Delivery Method reference data. " +
+                                "1 - Paper, " +
+                                "2 - Email, " +
+                                "3 - Digital, " +
                                 "99 - Other")]
         [Example(Description = "1")]
         public ActionPlanDeliveryMethod? ActionPlanDeliveryMethod { get; set; }
 
         [DataType(DataType.DateTime)]
-        [Display(Description = "Date and time the customer acknowledged receipt of the action plan. </br>" +
-                               "Validation Rules: </br>" +
+        [Display(Description = "Date and time the customer acknowledged receipt of the action plan " +
+                               "Validation Rules: " +
                                "DateActionPlanAcknowledged >= DateActionPlanCreated")]
         [Example(Description = "2018-06-22T07:55:00")]
         public DateTime? DateActionPlanAcknowledged { get; set; }
 
         [Required]
-        [Display(Description = "Priority Customer reference data. </br>" +
-                                "1 - 18 to 24 not in education, employment or training </br>" +
-                                "2 - Low skilled adults without a level 2 qualification </br>" +
-                                "3 - Adults who have been unemployed for more than 12 months </br>" +
-                                "4 - Single parents with at least one dependant child living in the same household </br>" +
-                                "5 - Adults with special educational needs and / or disabilities </br>" +
-                                "6 - Adults aged 50 years or over who are unemployed or at demonstrable risk of unemployment </br>" +
+        [Display(Description = "Priority Customer reference data. " +
+                                "1 - 18 to 24 not in education, employment or training " +
+                                "2 - Low skilled adults without a level 2 qualification " +
+                                "3 - Adults who have been unemployed for more than 12 months " +
+                                "4 - Single parents with at least one dependant child living in the same household " +
+                                "5 - Adults with special educational needs and / or disabilities " +
+                                "6 - Adults aged 50 years or over who are unemployed or at demonstrable risk of unemployment " +
                                 "99 - Not a priority customer")]
         [Example(Description = "1")]
         public PriorityCustomer? PriorityCustomer { get; set; }
