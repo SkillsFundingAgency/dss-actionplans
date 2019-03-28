1ba43468c38a36bc898c3d9eb628018f655008f8
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-03-28 08:40:36 +0000

    Update Description for Post and Patch Verbs

--- NCS.DSS.ActionPlan/APIDefinition/GenerateActionPlanSwaggerDoc.cs
+++ NCS.DSS.ActionPlan/APIDefinition/GenerateActionPlanSwaggerDoc.cs
@@ -16,12 +16,7 @@ namespace NCS.DSS.ActionPlan.APIDefinition
         public const string ApiDefRoute = ApiTitle + "/" + ApiDefinitionName;
         public const string ApiDescription = "To support the Data Collections integration with DSS, SessionId and SubcontractorId " +
                                              "attributes have been added and DateActionPlanCreated, DateAndTimeCharterShown, " +
-                                             "DateActionPlanSentToCustomer, DateActionPlanAcknowledged have new validation rules. <br>" + 
-                                             "<br><b>Validation Rules:</b> <br>" +
-                                             "<br><b>DateActionPlanCreated:</b> DateActionPlanCreated >= Session.DateAndTimeOfSession <br>" +
-                                             "<br><b>DateAndTimeCharterShown:</b> DateAndTimeCharterShown >= DateActionPlanCreated <br>" +
-                                             "<br><b>DateActionPlanSentToCustomer:</b> DateActionPlanSentToCustomer >= DateActionPlanCreated <br>" +
-                                             "<br><b>DateActionPlanAcknowledged:</b> DateActionPlanAcknowledged >= DateActionPlanCreated";
+                                             "DateActionPlanSentToCustomer, DateActionPlanAcknowledged have new validation rules.";
 
         public const string ApiVersion = "2.0.0";
       
