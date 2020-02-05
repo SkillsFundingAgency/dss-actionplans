e9729b07e3c26df01e04f535acbd7e3010141bba
Huss <huss@anefiox.com>
2020-02-05 10:51:55 +0000

    Changed swagger definition.

--- NCS.DSS.ActionPlan/APIDefinition/GenerateActionPlanSwaggerDoc.cs
+++ NCS.DSS.ActionPlan/APIDefinition/GenerateActionPlanSwaggerDoc.cs
@@ -14,11 +14,10 @@ namespace NCS.DSS.ActionPlan.APIDefinition
         public const string ApiTitle = "ActionPlans";
         public const string ApiDefinitionName = "API-Definition";
         public const string ApiDefRoute = ApiTitle + "/" + ApiDefinitionName;
-        public const string ApiDescription = "To support the Data Collections integration with DSS, SessionId and SubcontractorId " +
-                                             "attributes have been added and DateActionPlanCreated, DateAndTimeCharterShown, " +
-                                             "DateActionPlanSentToCustomer, DateActionPlanAcknowledged have new validation rules.";
+        public const string ApiDescription = "To support the Data Collections integration with DSS PriorityCustomer has been removed and is now " +
+            "recorded in the Customer API.";
 
-        public const string ApiVersion = "2.0.0";
+        public const string ApiVersion = "3.0.0";
       
         [FunctionName(ApiDefinitionName)]
         public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = ApiDefRoute)]HttpRequest req,
